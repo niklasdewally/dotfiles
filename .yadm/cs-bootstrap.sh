c7ff67144c7ef04e4a3a4bdf5bea1de1d6f019fc
Niklas Dewally <niklas.dewally@gmail.com>
2022-04-18 23:38:55 +0100

    CS Bootstrap: Fix path

--- .yadm/cs-bootstrap.sh
+++ .yadm/cs-bootstrap.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cp ~/.yadm-bootstrap/cs-profile.sh /cs/home/nd60/.profile.d/profile.sh
+cp ~/.yadm/cs-profile.sh /cs/home/nd60/.profile.d/profile.sh
 cp /cs/home/nd60/.ssh/id_rsa ~/.ssh/id_rsa
 cp /cs/home/nd60/.ssh/id_rsa.pub ~/.ssh/id_rsa.pub
 # The school overwrites these with symlinks
