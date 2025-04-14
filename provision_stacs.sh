6e0367a764407f0f703d21c3f99673789065fe71
Niklas Dewally <niklas@dewally.com>
2025-04-14 14:27:03 +0100

    stacs auto install rust_analyzer

--- provision_stacs.sh
+++ provision_stacs.sh
@@ -1,21 +1,29 @@
 #!/usr/bin/bash
 
+# initialises st-andrews lab machines with my dotfiles and a newer version of rust
+# (home directories reset on reboot)
+#
+# source this file
+
 $(return 0 > /dev/null 2>&1) || ( echo "please do not run this directly. source it instead." && exit 1 )
 
 echo "setting up dotfiles"
 
+# get rid of the defaults
 rm -rf ~/.bashrc ~/.bash_profile ~/.profile ~/.tmux.conf ~/.ipython ~/.bashrc.d ~/.gitconfig
 
+# load dotfiles from network home
 stow -t ${HOME} -d /cs/home/nd60/dotfiles common app-nvim device-stacs
 
+# apply them
 source ~/.profile
 source ~/.bashrc
-
 
 echo "..done!"
 
 echo "setting up rust"
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --no-modify-path
 source ~/.cargo/env
+rustup component add rust-analyzer
 echo "..done!"
 
