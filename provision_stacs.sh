4634ae390ef79f61b8941bdd9dbb433ea62d2fa0
Niklas Dewally <niklas@dewally.com>
2025-04-14 14:52:20 +0100

    stacs install ghcup

--- provision_stacs.sh
+++ provision_stacs.sh
@@ -27,3 +27,12 @@ source ~/.cargo/env
 rustup component add rust-analyzer
 echo "..done!"
 
+echo "setting up haskell"
+export BOOTSTRAP_HASKELL_NONINTERACTIVE=0 
+export BOOTSTRAP_HASKELL_INSTALL_HLS=1 
+curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sh
+unset BOOTSTRAP_HASKELL_NONINTERACTIVE
+unset BOOTSTRAP_HASKELL_INSTALL_HLS
+source ~/.ghcup/env
+echo "..done!"
+
