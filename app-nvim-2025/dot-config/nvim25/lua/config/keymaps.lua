7724fce6a4a455318dc1d8753ee4ccfd5ab45e22
Niklas Dewally <niklas@dewally.com>
2025-05-10 18:18:41 +0100

    nvim25: add zenmode

--- app-nvim-2025/dot-config/nvim25/lua/config/keymaps.lua
+++ app-nvim-2025/dot-config/nvim25/lua/config/keymaps.lua
@@ -61,3 +61,7 @@ vim.keymap.set('n', 'gK', function()
   local new_config = not vim.diagnostic.config().virtual_lines
   vim.diagnostic.config({ virtual_lines = new_config })
 end, { desc = 'Toggle diagnostic virtual_lines' })
+
+-- ZEN MODE
+
+vim.keymap.set('n','<leader>z', function() require("zen-mode").toggle() end, { desc = 'Toggle zen-mode' })
---
+++ app-nvim-2025/dot-config/nvim25/lua/plugins/zenmode.lua
@@ -0,0 +1,31 @@
+return {
+  "folke/zen-mode.nvim",
+  opts = {
+    window  = {
+      width = 0.85,
+      options = {
+        signcolumn = "no", -- disable signcolumn
+        number = false , -- disable number column
+        relativenumber = false, -- disable relative numbers
+        -- cursorline = false, -- disable cursorline
+        list = false, -- disable whitespace characters
+      },
+    },
+
+    plugins = {
+
+      -- disable some global vim options (vim.o...)
+      -- comment the lines to not apply the options
+      options = {
+        enabled = true,
+        ruler = false, -- disables the ruler text in the cmd line area
+        showcmd = true , -- disables the command in the last line of the screen
+        -- you may turn on/off statusline in zen mode by setting 'laststatus' 
+        -- statusline will be shown only if 'laststatus' == 3
+        laststatus = 0, -- turn off the statusline in zen mode
+      },
+
+      tmux = {enabled = true} -- disable tmux statusline when in zenmode
+    }
+  }
+}
