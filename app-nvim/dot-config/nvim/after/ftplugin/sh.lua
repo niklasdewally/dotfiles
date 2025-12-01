8441dd9d97820879d21bbc9ee56c3ab2659a4ea6
Nik Dewally <nik.dewally@arm.com>
2025-12-01 10:56:49 +0000

    fix shellcheck

--- app-nvim/dot-config/nvim/after/ftplugin/sh.lua
+++ app-nvim/dot-config/nvim/after/ftplugin/sh.lua
@@ -7,9 +7,13 @@ if vim.fn.executable("shfmt") then
 end
 
 if vim.fn.executable("shellcheck") then
+  local lint = require("lint")
+  -- customise shellcheck to add -x flag, allowing it to follow sources
+  local shellcheck = require("lint").linters.shellcheck
+  table.insert(shellcheck.args,0,"-x")
   vim.api.nvim_create_autocmd({ "BufWritePost" }, {
     callback = function()
-      require("lint").try_lint("shellcheck")
+      lint.try_lint("shellcheck")
     end,
   })
 end
