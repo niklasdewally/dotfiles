ec3e952f8e0658c7ed960f688ba61e3e16d56947
Niklas Dewally <niklas@dewally.com>
2026-02-08 16:30:58 +0000

    nvim: make lua_ls able to complete builtin vim libraries

---
+++ app-nvim/dot-config/nvim/lsp/lua_ls.lua
@@ -0,0 +1,26 @@
+-- see https://github.com/neovim/nvim-lspconfig/blob/master/lsp/lua_ls.lua for how to make lua_ls find nvim  libraries
+return {
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most
+        -- likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+        -- Tell the language server how to find Lua modules same way as Neovim
+        -- (see `:h lua-module-load`)
+        path = {
+          'lua/?.lua',
+          'lua/?/init.lua',
+        },
+      },
+      -- Make the server aware of Neovim runtime files
+      workspace = {
+        checkThirdParty = false,
+        library = {
+          vim.env.VIMRUNTIME,
+        },
+      }
+    }
+
+  }
+}
