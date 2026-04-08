9f093e95ccdbd5a075de71fe99b46a09900bc6fb
Nik Dewally <niklas@dewally.com>
2026-04-08 15:53:53 +0100

    plantuml lang support

---
+++ nvim/.config/nvim/plugin/41-plantuml.lua
@@ -0,0 +1,8 @@
+-- PlantUML language support 
+loadPlugins({
+  "https://github.com/aklt/plantuml-syntax", -- syntax and ftdetect
+
+  -- Preview plantuml files with :PlantumlOpen
+  "https://github.com/tyru/open-browser.vim.git",
+  "https://github.com/weirongxu/plantuml-previewer.vim.git" 
+})
